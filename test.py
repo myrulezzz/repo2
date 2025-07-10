da766c86b998ec9fc1451d1557884d8229b5a869
Andreas <43094013+myrulezzz@users.noreply.github.com>
2025-07-10 23:40:04 +0300

    Update test.py
    9565291938

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 from pandas import DataFrame, read_csv, concat, to_datetime, to_numeric
 # I wiil use alway pandas Anything else is not needed.
+from confluent_kafka import KafkaProducer, KafkaConsumer, KafkaError
